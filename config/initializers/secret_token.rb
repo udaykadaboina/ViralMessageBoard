1684c9318fdeb73c91797b5c81ae51a1d89a42bf
Uday <uday88k@gmail.com>
2013-08-03 16:19:16 -0600

    created Posts model with rspec config

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ViralMessageBoard::Application.config.secret_key_base = 'bc8c2b37fe2b5a36519e54b7a601e7c612115def0ea5965e4cf7a6099d6136777bb0e3c780f55461a641161835449c4ae75cb8d40ff880c1274fe8c575e80cd1'
